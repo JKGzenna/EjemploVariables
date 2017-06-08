a9321e19b7dcd4fe30a879ffc8fb9e055b0b9c9d
JKGzenna <juanlu360@gmail.com>
2017-06-09 01:37:02 +0200

    EjemploVariables

---
+++ src/ejemplovariables/EjemploVariables.java
@@ -0,0 +1,22 @@
+package ejemplovariables;
+
+/**
+ *
+ * @author JUAN
+ */
+public class EjemploVariables {
+
+    public static void main(String[] args) {
+        
+        int a;
+        int b = 0; // inicialmente se declara en 0
+        
+        a = 5;
+        b = 7;
+        
+        double numeroReal = 7.4;
+        
+        System.out.println(a+b+numeroReal);
+    }
+    
+}
